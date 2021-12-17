a2a3d09b782771382cb801694dd739bfe879f2c9
duyvk <duyvk@chromium.org>
2021-12-17 16:37:15 +0700

    update src of data

--- src/day1_2021_sonic_sweep/Source.cpp
+++ src/day1_2021_sonic_sweep/Source.cpp
@@ -13,6 +13,7 @@ int32_t CountIncreaseMeasurements(std::vector<int32_t> sonic_sweep) {
 int32_t main() {
     int32_t      number;
     std::fstream input;
+    //data.txt cotain input taken from https://adventofcode.com/2021/day/1/input
     input.open("data.txt", std::fstream::in);
     std::vector<int> sonic_sweep;
     while (input >> number)
